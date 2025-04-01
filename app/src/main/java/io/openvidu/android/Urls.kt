b842914b805e84bf6dab35493bbf1355d680fea9
shubhamtaneja <shubhamtaneja@uniqus.com>
2025-04-01 22:32:15 +0530

    android application

---
+++ app/src/main/java/io/openvidu/android/Urls.kt
@@ -0,0 +1,8 @@
+package io.openvidu.android
+
+object Urls {
+    // Configure these variables with correct URLs depending on your deployment
+    // If you leave them empty, the user will be prompted to enter the URLs
+    var applicationServerUrl = "https://cloud-api.livekit.io/api/sandbox/connection-details"
+    var livekitUrl = "wss://aivisionapp-y34s2dz8.livekit.cloud"
+}
